affc25bf88bf1b658da8c65503c9d70303aec0ee
kidist tadesse <k44144202@gmail.com>
2025-08-11 08:39:07 +0300

    Update eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -20,6 +20,8 @@ export default defineConfig([
         ecmaVersion: 'latest',
         ecmaFeatures: { jsx: true },
         sourceType: 'module',
+        sourceType: 'module',
+        
       },
     },
     rules: {
