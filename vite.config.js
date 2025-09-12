03585e0bcf4c46358832708d1e0a52aa0db1a6bb
kidist tadesse <k44144202@gmail.com>
2025-09-12 14:39:27 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: "/-YouTube-Clone-React-YouTube-API-v3/",
+  base: "/",
   // 👈 This must match your repo name
   plugins: [react()],
 })
